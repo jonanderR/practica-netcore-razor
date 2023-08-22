4e637495738ce72b23634693cef1f61c8222b0e8
JonAnder <jonander.ruiz@versia.com>
2023-08-22 10:17:43 +0200

    Modelo y esquema de BD con EF

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+    }
+}
