509c775bb53c6d589b400daab7c17b4a37cd8061
JonAnder <jonander.ruiz@versia.com>
2023-08-29 12:51:25 +0200

    Agrego validaciones

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -8,13 +8,16 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3),Required]
         public string Title { get; set; } = string.Empty;
 
-        [DataType(DataType.Date),Display(Name = "Release Date")]
+        [DataType(DataType.Date),Display(Name = "Release Date"), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"),Required, StringLength(30)]
         public string Genre { get; set; } = string.Empty;
-        [Column(TypeName ="decimal(18,2)")]
+        [Column(TypeName ="decimal(18,2)"),Range(1,100),DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; } = string.Empty;
     }
 }
